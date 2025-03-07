f31740368f43dbb6f5d689d5485cd6287b03eed8
Mehmet Baran Özdeniz <ozdenizm6@gmail.com>
2025-03-07 16:09:55 +0300

    Dropwizard'ta Flyway ile veritabanı tablosunun nasıl olusturulacagi ogrenildi

--- src/main/java/com/user/UserApplication.java
+++ src/main/java/com/user/UserApplication.java
@@ -3,6 +3,7 @@ package com.user;
 import io.dropwizard.core.Application;
 import io.dropwizard.core.setup.Bootstrap;
 import io.dropwizard.core.setup.Environment;
+import org.flywaydb.core.Flyway;
 
 public class UserApplication extends Application<UserConfiguration> {
 
@@ -23,7 +24,13 @@ public class UserApplication extends Application<UserConfiguration> {
     @Override
     public void run(final UserConfiguration configuration, final Environment environment) {
         // TODO: implement application
-
+        Flyway flyway = Flyway.configure()
+                .dataSource(configuration.getDatabase().getUrl(),
+                        configuration.getDatabase().getUser(),
+                        configuration.getDatabase().getPassword())
+                .locations(configuration.getFlywayFactory().getLocations().toArray(new String[0]))
+                .load();
+        flyway.migrate();
     }
 
 }
--- src/main/java/com/user/UserConfiguration.java
+++ src/main/java/com/user/UserConfiguration.java
@@ -2,9 +2,36 @@ package com.user;
 
 import io.dropwizard.core.Configuration;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import org.hibernate.validator.constraints.*;
-import jakarta.validation.constraints.*;
+import io.dropwizard.db.DataSourceFactory;
+import io.dropwizard.flyway.FlywayFactory;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 
 public class UserConfiguration extends Configuration {
     // TODO: implement service configuration
+    @Valid
+    @NotNull
+    @JsonProperty("database")
+    private DataSourceFactory database;
+
+    @Valid
+    @NotNull
+    @JsonProperty("flyway")
+    private FlywayFactory flyway;
+
+    public DataSourceFactory getDatabase() {
+        return database;
+    }
+
+    public void setDatabase(DataSourceFactory database) {
+        this.database = database;
+    }
+
+    public FlywayFactory getFlywayFactory() {
+        return flyway;
+    }
+
+    public void setFlywayFactory(FlywayFactory flyway) {
+        this.flyway = flyway;
+    }
 }
