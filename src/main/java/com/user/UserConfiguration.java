421cbd1f906547da9563087c47395192dee30682
Mehmet Baran Özdeniz <ozdenizm6@gmail.com>
2025-03-10 16:48:59 +0300

    database ve flyway konfigurasyonlari yapildi

--- src/main/java/com/user/UserConfiguration.java
+++ src/main/java/com/user/UserConfiguration.java
@@ -1,6 +1,38 @@
 package com.user;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.dropwizard.core.Configuration;
+import io.dropwizard.db.DataSourceFactory;
+import io.dropwizard.flyway.FlywayFactory;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 
 public class UserConfiguration extends Configuration {
+
+    @Valid
+    @NotNull
+    @JsonProperty("database")
+    private DataSourceFactory database;
+
+    @Valid
+    @NotNull
+    @JsonProperty("flyway")
+    private FlywayFactory flyway;
+
+    public void setDatabase(DataSourceFactory database) {
+        this.database = database;
+    }
+
+    public DataSourceFactory getDatabase() {
+        return database;
+    }
+
+    public void setFlyway(FlywayFactory flyway) {
+        this.flyway = flyway;
+    }
+
+    public FlywayFactory getFlyway() {
+        return flyway;
+    }
+
 }
